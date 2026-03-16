1a2bd11c850592b07badd63d4fc3f1786986ad54
matsozza <matheussozza@hotmail.com>
2026-03-15 22:32:35 -0300

    logging adjustments, runtime sequencing and data structuring

--- src/weather-station.py
+++ src/weather-station.py
@@ -8,6 +8,7 @@ import time
 import logging
 import signal
 import requests
+import os
 from datetime import datetime
 
 from bmp280.bmp280 import read_bmp280_pipe
@@ -20,19 +21,30 @@ from firebase_admin import credentials, firestore
 # Configuration
 # ==============================
 POLL_INTERVAL_SEC = 60
-LOG_FILE = "/home/matheus/weather.log"
+LOCATION_POLL_CYCLES = 60*24 # Once a day
+
 FIREBASE_KEY = "./database_key.json"
-COLLECTION_NAME = "weather-data-sjc"
+COLLECTION_NAME = "weather-data-raw"
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_FILE = os.path.join(BASE_DIR, "weather-station.log")
 
 # ==============================
 # Setup Logging
 # ==============================
-logging.basicConfig(
-    filename=LOG_FILE,
-    level=logging.ERROR,
-    format="%(asctime)s %(levelname)s: %(message)s"
-)
-logger = logging.getLogger("WeatherService")
+
+# Check if we can write to it (if not, remove it)
+if not os.access(LOG_FILE, os.W_OK) and os.path.exists(LOG_FILE):
+    os.remove(LOG_FILE)
+
+# Recreate it (it will be empty and owned by the current user)
+with open(LOG_FILE, "w") as f:
+    f.write("Log started\n")
+os.chmod(LOG_FILE, 0o666) # Ensure suitable permissions for everyone
+
+logger = logging.getLogger("WeatherStation")
+logger.setLevel(logging.INFO)
+logger.propagate = False
 
 # ==============================
 # Graceful Shutdown
@@ -54,6 +66,7 @@ def init_firebase():
     logger.info("Initializing Firebase...")
     cred = credentials.Certificate(FIREBASE_KEY)
     firebase_admin.initialize_app(cred)
+    logger.info("Firebase initialized")
     return firestore.client()
 
 # ==============================
@@ -70,12 +83,12 @@ def read_sensors():
         logger.info(f"DHT22: {dht22}")
         logger.info(f"BMP280: {bmp280}")
         return {
-            "temperature_dht22": dht22.temperature / 10,
-            "humidity_dht22": dht22.humidity / 10,
-            "validity_dht22": dht22.validity,
-            "temperature_bmp280": bmp280.temperature,
-            "pressure_bmp280": bmp280.pressure,
-            "validity_bmp280": bmp280.validity,
+            "data_temp_dht22": dht22.temperature / 10,
+            "data_humi_dht22": dht22.humidity / 10,
+            "data_valid_dht22": dht22.validity,
+            "data_temp_bmp280": bmp280.temperature,
+            "data_pres_bmp280": bmp280.pressure,
+            "data_valid_bmp280": bmp280.validity,
         }
     except Exception as e:
         logger.error(f"Failed to read sensors: {e}")
@@ -91,20 +104,20 @@ def get_location():
         data = response.json()
         loc = data.get("loc", "").split(",")
         return {
-            "city": data.get("city"),
-            "region": data.get("region"),
-            "country": data.get("country"),
-            "lat": float(loc[0]),
-            "lon": float(loc[1])
+            "loc_city": data.get("city"),
+            "loc_region": data.get("region"),
+            "loc_country": data.get("country"),
+            "loc_lat": float(loc[0]),
+            "loc_lon": float(loc[1])
         }
     except Exception as e:
         logger.error(f"Failed to get location: {e}")
         return {
-            "city": "Error - No Data",
-            "region": "Error - No Data",
-            "country": "Error - No Data",
-            "lat": "Error - No Data",
-            "lon": "Error - No Data"
+            "loc_city": "Error - No Data",
+            "loc_region": "Error - No Data",
+            "loc_country": "Error - No Data",
+            "loc_lat": "Error - No Data",
+            "loc_lon": "Error - No Data"
         }
 
 # ==============================
@@ -126,12 +139,23 @@ def push_to_firebase(database, data):
 def main():
     logger.info("Weather Service started.")
     database = init_firebase()
-
+    location = get_location()
+    
+    # First read - discard data, hardware initializing
+    read_sensors() 
+    
+    time.sleep(5)
+    
+    cycles = 0
     while running:
-        location = get_location()
         data = read_sensors()
         push_to_firebase(database, {**data, **location})
         time.sleep(POLL_INTERVAL_SEC)
+        cycles = cycles + 1
+        
+        # Update location periodically
+        if cycles % LOCATION_POLL_CYCLES == 0:
+            location = get_location()                        
 
     logger.info("Weather Service stopped.")
 
