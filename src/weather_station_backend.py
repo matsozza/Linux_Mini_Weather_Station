fb45c3b426782e3a47ec8c662b6a6426cb3808fb
matsozza <matheussozza@hotmail.com>
2026-03-16 14:13:40 -0300

    backend iteration

--- src/weather_station_backend.py
+++ src/weather_station_backend.py
@@ -9,7 +9,8 @@ class WeatherStationBackend:
     def __init__(self):
         # Use the same key path as your weather_service.py
         self.db_key = "./database_key.json"
-        self.collection_name = "weather-data-raw"
+        self.collection_name_raw = "weather-data-raw"
+        self.collection_name_hourly = "weather-data-hourly"
         
         # Initialize Firebase only if not already initialized
         if not firebase_admin._apps:
@@ -33,7 +34,7 @@ class WeatherStationBackend:
 
         # Example to get collection size only
         query = (
-            self.db.collection(self.collection_name)
+            self.db.collection(self.collection_name_raw)
             .where(filter=FieldFilter("timestamp", ">=", start_time))
             .order_by("timestamp")
         )
@@ -43,7 +44,7 @@ class WeatherStationBackend:
 
         # Get pointer to data stream
         docs = (
-            self.db.collection(self.collection_name)
+            self.db.collection(self.collection_name_raw)
             .where(filter=FieldFilter("timestamp", ">=", start_time))
             .order_by("timestamp")
             .stream()
@@ -61,73 +62,85 @@ class WeatherStationBackend:
 
         return pd.DataFrame(data_list).sort_values("timestamp")
     
-    def process_hourly_summary(self):
+    def process_hourly_summary(self, prev_hours = 24):
             """
             Sweeps the last hour(s) of raw data, calculates stats, 
             and saves to 'weather-data-hourly'.
             """
+            
+            target_doc_ids = []
             now = datetime.now().astimezone()
-            last_hour = now.replace(minute=0, second=0, microsecond=0) - timedelta(hours=1)
-            curr_hour = now.replace(minute=0, second=0, microsecond=0)
-
-            print(f"Summarizing data from {last_hour} to {curr_hour}...")
-
-            # 1. Fetch data for this specific window
-            # We reuse the logic but narrow the scope to exactly 1 hour
-            query = (
-                self.db.collection(self.collection_name)
-                .where(filter=FieldFilter("timestamp", ">=", last_hour))
-                .where(filter=FieldFilter("timestamp", "<=", curr_hour))
-                .order_by("timestamp")
-                .stream()
-            )
-
-            data_list = []
-            for doc in query:
-                data = doc.to_dict()
-                data_list.append(data)
-
-            if not data_list:
-                print("No data found for the last hour. Skipping summary.")
-                return
-
-            # Create dataframe and remove invalid rows
-            df = pd.DataFrame(data_list)
-            cols = ["data_valid_bmp280", "data_valid_dht22"]  
-            df = df[df[cols].all(axis=1)]
-
-            # 2. Calculate Stats
+            for prev_hour in range(1, prev_hours):
+                target_time = (now - timedelta(hours = prev_hour)).replace(minute=0, second=0, microsecond=0)
+                target_doc_ids.append(  target_time.strftime("%Y-%m-%d__%H-%M-%S__%z")  )
             
-            summary = {
-                "timestamp_processed": now,
-                "window_start": last_hour,
-                "window_end": curr_hour,
-                "sample_count": len(df),
-                "data_timestamps": list(df["timestamp"])
-            }
-
-            for col in df.columns:
-                if col.startswith("data_"):
-                    if col.startswith("data_valid"):
-                        pass # Ignore validities since already filtered in dataframe
-                    else:
-                        summary[f"{col}_avg"] = float(df[col].mean())
-                        summary[f"{col}_min"] = float(df[col].min())
-                        summary[f"{col}_max"] = float(df[col].max())
-                        summary[f"{col}_values"] = list(df[col])
-                else:
-                    if col == "timestamp":
-                        pass # Ignore timestamp
-                    else:
-                        # Take the first non-null value from the column
-                        summary[col] = df[col].dropna().iloc[0]
-
-            # 3. Save to Firestore
-            # Using the hour's start time as the Document ID keeps things organized
-            doc_id = last_hour.strftime("%Y-%m-%d__%H-%M-%S__%z")
-            self.db.collection("weather-data-hourly").document(doc_id).set(summary)
+            found_doc_ids = [
+                    self.db.collection(self.collection_name_hourly).document(doc_id).get()
+                    for doc_id in target_doc_ids
+            ]
+            found_doc_ids = [d.id for d in found_doc_ids if d.exists]
             
-            print(f"Successfully saved summary to 'weather-data-hourly' with ID: {doc_id}")
+            # Update target docs with only the ones that aren't present in the DB
+            target_doc_ids = [doc for doc in target_doc_ids if doc not in found_doc_ids]
+
+            for target_doc in target_doc_ids:
+                time_range_init = datetime.strptime(target_doc, "%Y-%m-%d__%H-%M-%S__%z")
+                time_range_fin = datetime.strptime(target_doc, "%Y-%m-%d__%H-%M-%S__%z") + timedelta(hours=1)
+
+                print(f"Summarizing data from {time_range_init} to {time_range_fin}...")
+                
+                # Fetch raw documents to aggregate in hourly collection
+                found_raw_doc_ids = (
+                    self.db.collection(self.collection_name_raw)
+                    .where(filter=FieldFilter("timestamp", ">=", time_range_init))
+                    .where(filter=FieldFilter("timestamp", "<=", time_range_fin))
+                    .order_by("timestamp")
+                    .stream()
+                )
+
+                raw_data_list = []
+                for raw_doc in found_raw_doc_ids:
+                    data = raw_doc.to_dict()
+                    raw_data_list.append(data)
+                if not raw_data_list:
+                    print(f"No data found for {target_doc}. Skipping summary for this one.")
+                    continue
+
+                # Create dataframe and remove invalid rows
+                df = pd.DataFrame(raw_data_list)
+                cols = ["data_valid_bmp280", "data_valid_dht22"]  
+                df = df[df[cols].all(axis=1)]
+
+                # 2. Calculate Stats                
+                summary = {
+                    "timestamp_processed": now,
+                    "window_start": time_range_init,
+                    "window_end": time_range_fin,
+                    "sample_count": len(df),
+                    "data_timestamps": list(df["timestamp"])
+                }
+
+                for col in df.columns:
+                    if col.startswith("data_"):
+                        if col.startswith("data_valid"):
+                            pass # Ignore validities since already filtered in dataframe
+                        else:
+                            summary[f"{col}_avg"] = float(df[col].mean())
+                            summary[f"{col}_min"] = float(df[col].min())
+                            summary[f"{col}_max"] = float(df[col].max())
+                            summary[f"{col}_values"] = list(df[col])
+                    else:
+                        if col == "timestamp":
+                            pass # Ignore timestamp
+                        else:
+                            # Take the first non-null value from the column
+                            summary[col] = df[col].dropna().iloc[0]
+
+                # 3. Save to Firestore
+                # Using the hour's start time as the Document ID keeps things organized
+                self.db.collection("weather-data-hourly").document(target_doc).set(summary)
+                
+                print(f"Successfully saved summary to 'weather-data-hourly' with ID: {target_doc}")
     
 if __name__ == "__main__":
     # --- DEBUGGING / TESTING BLOCK ---
